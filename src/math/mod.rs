d6c965cdaf477c6ca70c210db1860fb9e8bf68c2
Wirandhika Maulana Akbar <052104692@ecampus.ut.ac.id>
2025-07-23 03:00:46 +0700

    Update mod.rs

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,4 +1,4 @@
-/// Modul utama matematika (math)
+/// Modul utama matematika
 /// Main mathematics module
 
 pub mod aritmetika;      // Aritmetika dasar / Basic arithmetic
@@ -7,10 +7,14 @@ pub mod trigonometri;    // Trigonometri / Trigonometry
 pub mod geometri;        // Geometri / Geometry
 pub mod statistika;      // Statistika / Statistics
 pub mod kombinatorika;    // Kombinatorik & Peluang / Combinatorics & Probability
+pub mod basis;  // Konversi basis bilangan / Number base conversion
 
+// Re-export semua modul utama agar bisa diakses langsung dari math::...
+// Re-export all main modules for direct access via math::...
 pub use aritmetika::*;
 pub use aljabar::*;
 pub use trigonometri::*;
 pub use geometri::*;
 pub use statistika::*;
 pub use kombinatorika::*;
+pub use basis::*;
